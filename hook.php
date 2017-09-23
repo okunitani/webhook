1b143a1683542034b761b58d827fbf1224f5b71c
okunitani <okunitani@gmail.com>
2017-09-23 18:15:40 +0900

    メモ

--- hook.php
+++ hook.php
@@ -1,10 +1,11 @@
 <?php
 
 $LOG_FILE = dirname(__FILE__).'/hook.log';
-$SECRET_KEY = 'R9vfMdVEotYDXK';
+$SECRET_KEY = 'R9vfMdVEotYDXK'; // secret_keyを用意し、secret_keyが一致するときのみ処理を行う
 
 if ( isset($_GET['key']) && $_GET['key'] === $SECRET_KEY && isset($_POST['payload']) ) {
     $payload = json_decode($_POST['payload'], true);
+    // githubはpayloadとうパラメータでコミットに関する情報をPOSTしてくるので、これを見てmasterの時のみgitのpullを行う
     if ($payload['ref'] === 'refs/heads/master') {
         exec(`~/opt/bin/git pull`);
         file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." ".$_SERVER['REMOTE_ADDR']." git pulled: ".$payload['head_commit']['message']."\n", FILE_APPEND|LOCK_EX);
