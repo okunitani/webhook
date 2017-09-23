c29e0c39b5edda4f51d2de1a612f507f08766e0b
okunitani <okunitani@gmail.com>
2017-09-23 15:39:03 +0900

    test

--- hook.php
+++ hook.php
@@ -6,7 +6,7 @@ $SECRET_KEY = 'R9vfMdVEotYDXK';
 if ( isset($_GET['key']) && $_GET['key'] === $SECRET_KEY && isset($_POST['payload']) ) {
     $payload = json_decode($_POST['payload'], true);
     if ($payload['ref'] === 'refs/heads/master') {
-        exec('git pull 2>&1');
+        exec('cd ~/okunitani.xsrv.jp/public_html/webhook/; git pull');
         file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." ".$_SERVER['REMOTE_ADDR']." git pulled: ".$payload['head_commit']['message']."\n", FILE_APPEND|LOCK_EX);
     }
 } else {
